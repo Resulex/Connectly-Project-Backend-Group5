197318cb65c34e1984296417b74839d3ee5e7d94
Alex Resurreccion <alexresu@Alexs-MacBook-Air.local>
2026-03-02 13:35:17 +0800

    feat: Implement news feed with caching and pagination

--- posts/pagination.py
+++ posts/pagination.py
@@ -1,7 +1,14 @@
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 class Pagination(PageNumberPagination):
     page_size = 5
     page_size_query_param = 'page_size'
     max_page_size = 50
+
+    def get_paginated_response(self, data):
+        return Response({
+            'total_count': self.page.paginator.count,
+            'results': data
+        })
 
